79bbffbe831aa58d34a326582325800301e302b0
F van Gastel <felicia.vangastel@student.uva.nl>
2018-05-11 17:49:53 +0200

    only view of map and tooltip

--- homework/week_5/js/main.js
+++ homework/week_5/js/main.js
@@ -4,9 +4,9 @@ window.onload = function() {
     	.defer(d3.json, "data/data2006.json")
     	.defer(d3.json, "data/data2016.json")
       .defer(d3.json, "data/nld.json")
-    	.await(makeMyMap);
+      .await(makeMap);
 
-    function makeMyMap () {
+    function makeMap () {
         if (error) throw error;
 
     }
--- homework/week_5/js/mapNL.js
+++ homework/week_5/js/mapNL.js
@@ -1,52 +1,64 @@
 // http://bl.ocks.org/phil-pedruco/9344373
 
-var width = 500,
-    height = 500;
+// window.onload = function() {
 
-var colour = d3.scale.category20();
+  // queue()
+  //       .defer(d3.json, "data/nld.json")
+  //       .defer(d3.json, "data/data2006.json")
+  //       .defer(d3.json, "data/data2016.json")
+  //       .await(makeMap);
 
-var projection = d3.geo.mercator()
-    .scale(1)
-    .translate([0, 0]);
+  // function makeMap (error, nld, data2006, data2016) {
 
-var path = d3.geo.path()
-    .projection(projection);
+      var width = 500,
+          height = 500;
 
-var svg = d3.select("body").append("svg")
-    .attr("class", "map")
-    .attr("width", width)
-    .attr("height", height);
+      var colour = d3.scale.category20();
 
-var mapTip = d3.tip()
-  .attr('class', 'd3-tip')
-  .offset([-10, 0])
-  .html(function(d) {
-    return "Population:";
-  })
+      var projection = d3.geo.mercator()
+          .scale(1)
+          .translate([0, 0]);
 
-d3.json("data/nld.json", function(error, nld) {
+      var path = d3.geo.path()
+          .projection(projection);
 
-    var l = topojson.feature(nld, nld.objects.subunits).features[3],
-        b = path.bounds(l),
-        s = .2 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
-        t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+      var svg = d3.select("body").append("svg")
+          .attr("class", "map")
+          .attr("width", width)
+          .attr("height", height);
 
-    projection
-        .scale(s)
-        .translate(t);
+      var mapTip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+          return "<strong>Population:</strong>"
+        });
 
-    svg.selectAll("path")
-        .data(topojson.feature(nld, nld.objects.subunits).features).enter()
-        .append("path")
-        .attr("d", path)
-        .attr("fill", function(d, i) {
-            return colour(i);
-        })
-        .attr("class", function(d, i) {
-            return d.properties.name;
-        })
-        // create mouse events
-        .call(mapTip)
-        .on('mouseover', mapTip.show) // interactivity
-        .on('mouseout', mapTip.hide);
-});
+  d3.json("data/nld.json", function(error, nld) {
+
+      var l = topojson.feature(nld, nld.objects.subunits).features[3],
+          b = path.bounds(l),
+          s = .2 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height),
+          t = [(width - s * (b[1][0] + b[0][0])) / 2, (height - s * (b[1][1] + b[0][1])) / 2];
+
+      projection
+          .scale(s)
+          .translate(t);
+
+      svg.selectAll("path")
+          .data(topojson.feature(nld, nld.objects.subunits).features).enter()
+          .append("path")
+          .attr("d", path)
+          .attr("fill", function(d, i) {
+              return colour(i);
+          })
+          .attr("class", function(d, i) {
+              return d.properties.name;
+          })
+          // create mouse events
+          .call(mapTip)
+          .on('mouseover', mapTip.show) // interactivity
+          .on('mouseout', mapTip.hide);
+  });
+  // };
+// };
