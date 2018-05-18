c5234894abb9aafc0c4ccc62455416f496d4bf35
F van Gastel <felicia.vangastel@student.uva.nl>
2018-05-19 00:45:54 +0200

    cleaner code

--- homework/week_5/js/piechart.js
+++ homework/week_5/js/piechart.js
@@ -3,110 +3,116 @@
 * 11096187
 *
 * piechart.js
-* script for a piechart
+* script that makes a piechart visualization
 *
+* sources:
+* https://bl.ocks.org/mbostock/3887235
+* http://bl.ocks.org/NPashaP/96447623ef4d342ee09b
 */
 
-//https://bl.ocks.org/mbostock/3887235
-//http://bl.ocks.org/NPashaP/96447623ef4d342ee09b
-
+// set up global variable to use in main
 var updatePie;
 
+// function that makes the pie chart
 function makePie(data) {
 
-  var margin = {top: 100, bottom: 100, left: 100, right: 100},
-      h = 500 - margin.top - margin.bottom,
-      w = 500 - margin.left - margin.right;
+    // set up margin for pie chart
+    var margin = {top: 100, bottom: 100, left: 100, right: 100},
+        h = 500 - margin.top - margin.bottom,
+        w = 500 - margin.left - margin.right;
 
-  // var w = 250;
-  // var h = 250;
-  var r = h/2;
-  var aColor = d3.scale.category10();
+    // set up radius for pie
+    var r = h/2;
 
-  // loop over objects
-  for (let i = 0; i < 12; i++){
-      var obj = data[i].popGroups;
-      var objectKeys = Object.keys(obj); // => ["name", "surname"]
-      // objectKeys.forEach(function(d){
-      // });
-      console.log(obj);
-      console.log(objectKeys);
-  }
+    // set up color palette
+    var aColor = d3.scale.category10();
 
-  // create svg for pie chart
-  var piesvg = d3.select('.pie').append("svg:svg")
-      .data([obj])
-      .attr("width", w)
-      .attr("height", h)
-      .append("svg:g")
-      .attr("transform", "translate(" + r + "," + r + ")");
+    // loop over objects and store object groups
+    for (let i = 0; i < 12; i++){
+        var obj = data[i].popGroups;
+        var objectKeys = Object.keys(obj);
+    };
 
-  // create a function to compute the pie slice angles
-  var pie = d3.layout.pie()
-      .value(function(d){return d.value;});
+    // create svg for pie chart
+    var piesvg = d3.select('.pie').append("svg:svg")
+        .data([obj])
+        .attr("width", w)
+        .attr("height", h)
+        .append("svg:g")
+        .attr("transform", "translate(" + r + "," + r + ")");
 
-  // create function to draw the arcs of the pie slices
-  var arc = d3.svg.arc().outerRadius(r);
+    // create a function to compute the pie slice angles
+    var pie = d3.layout.pie()
+        .value(function(d){return d.value;});
 
-  // select paths, use arc generator to draw
-  var arcs = piesvg.selectAll("g.slice")
-      .data(pie).enter()
-      .append("svg:g").attr("class", "slice");
-  arcs.append("svg:path")
-      .attr("fill", function(d, i){ return aColor(i); })
-      .attr("d", function (d) { return arc(d); })
-      .each(function(d) { this._current = d; })
+    // create function to draw the arcs of the pie slices
+    var arc = d3.svg.arc().outerRadius(r);
 
-  document.getElementById("pieTitle").innerHTML = " Choose Province!";
+    // select paths, use arc generator to draw
+    var arcs = piesvg.selectAll("g.slice")
+        .data(pie).enter()
+        .append("svg:g").attr("class", "slice");
+    arcs.append("svg:path")
+        .attr("fill", function(d, i){ return aColor(i); })
+        .attr("d", function (d) { return arc(d); })
+        .each(function(d) { this._current = d; })
 
-  // var pieLegend = piesvg.selectAll(".legend")
-  //     .data([obj])
-  //   .enter().append("g")
-  //     .attr("class", "legend")
-  //     .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-  //
-  // pieLegend.append("rect")
-  //   .attr("x", w)
-  //   .attr("width", 18)
-  //   .attr("height", 18)
-  //   .style("fill", aColor);
-  //
-  // pieLegend.append("text")
-  //   .attr("x", w + 25)
-  //   .attr("y", 9)
-  //   .attr("dy", ".35em")
-  //   .style("text-anchor", "start")
-  //   .text(18, 5, 11);
+    // set up initial title for pie chart
+    document.getElementById("pieTitle").innerHTML = " Choose Province!";
 
-  function updatePies(province) {
-      var pie = d3.layout.pie()
-          .value(function(d){return d.value;});
+    // set up legend
+    var pieLegend = piesvg.selectAll(".legend")
+        .data([obj])
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
-      document.getElementById("pieTitle").innerHTML = " Age groups in " + province.bold();
+    // append rectangle to legend
+    pieLegend.append("rect")
+      .attr("x", w)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", aColor);
 
-      console.log(province);
+    // append text to legend
+    pieLegend.append("text")
+      .attr("x", w + 25)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "start")
+      .text(18, 5, 11);
 
-      // get data from clicked province
-      var dataNew;
-      data.forEach(function(d){
-          if (d.province == province) {
-              dataNew = d.popGroups;
+    // function that will update the pie to the correct province
+    function updatePies(province) {
+        var pie = d3.layout.pie()
+            .value(function(d){return d.value;});
+
+        // change title to chosen province
+        document.getElementById("pieTitle").innerHTML = " Age groups in " + province.bold();
+
+        // get new data from clicked province
+        var dataNew;
+        data.forEach(function(d){
+            if (d.province == province) {
+                dataNew = d.popGroups;
+            };
+        });
+
+        // set up the transition of the pie, using actTween
+        d3.select(".pie").select("svg").selectAll("path") //path
+            .data(pie(dataNew)).transition().duration(500)
+            .attrTween("d", arcTween);
+
+        // Store the displayed angles in _current.
+        // Then, interpolate from _current to the new angles.
+        // During the transition, _current is updated in-place by d3.interpolate.
+        function arcTween(a) {
+            var i = d3.interpolate(this._current, a)
+            this._current = i(0);
+            return function(t) { return arc(i(t)); };
         }
-      })
-      console.log(dataNew);
+    };
 
-      d3.select(".pie").select("svg").selectAll("path") //path
-          .data(pie(dataNew)).transition().duration(500)
-          .attrTween("d", arcTween);
-
-      // Store the displayed angles in _current.
-      // Then, interpolate from _current to the new angles.
-      // During the transition, _current is updated in-place by d3.interpolate.
-      function arcTween(a) {
-          var i = d3.interpolate(this._current, a)
-          this._current = i(0);
-          return function(t) { return arc(i(t)); };
-      }
-  };
-  updatePie = updatePies;
+    // makes it able to use the update function in another javascript
+    updatePie = updatePies;
 };
